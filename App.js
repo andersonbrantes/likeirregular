d4cafc6d28e5c11faa5b4c215e9222486e2ef81a
Anderson P. Brantes <andersonbrantes@gmail.com>
2019-02-23 11:41:18 -0300

    Creates and Adds a TipsBlock component with a card template example.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Title, Footer, FooterTab, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import { Container, Header, Content, Title, Footer, FooterTab, Text, Button, Icon, Left, Body, Right } from 'native-base';
+
+import { TipsBlock } from './components/TipsBlock';
 
 export default class App extends Component {
   constructor() {
@@ -9,9 +11,11 @@ export default class App extends Component {
       isReady: false
     };
   }
+
   componentWillMount() {
     this.loadFonts();
   }
+  
   async loadFonts() {
     await Expo.Font.loadAsync({
       Roboto: require("./node_modules/native-base/Fonts/Roboto.ttf"),
@@ -27,12 +31,6 @@ export default class App extends Component {
 
     return (
       <Container>
-        <Content>
-          <Text>
-            This is Content Section
-          </Text>
-        </Content>
-
         <Header>
           <Left>
             <Button transparent>
@@ -44,43 +42,8 @@ export default class App extends Component {
           </Body>
           <Right />
         </Header>
-{/*         <Content>
-          <Text>
-            This is Content Section
-          </Text>
-        </Content> */}
         <Content>
-          <Card>
-              <CardItem>
-                <Left>
-                  <Thumbnail source={{uri: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&imgrefurl=https://www.pexels.com/search/nature/&docid=ShwNVOdFBcmkxM&tbnid=8c_UAo3gH_220M:&vet=1&w=500&h=200&source=sh/x/im'}} />
-                  <Body>
-                    <Text>NativeBase</Text>
-                    <Text note>GeekyAnts</Text>
-                  </Body>
-                </Left>
-              </CardItem>
-              <CardItem cardBody>
-                <Image source={{uri: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&imgrefurl=https://www.pexels.com/search/nature/&docid=ShwNVOdFBcmkxM&tbnid=8c_UAo3gH_220M:&vet=1&w=500&h=200&source=sh/x/im'}} style={{height: 200, width: null, flex: 1}}/>
-              </CardItem>
-              <CardItem>
-                <Left>
-                  <Button transparent>
-                    <Icon active name="thumbs-up" />
-                    <Text>12 Likes</Text>
-                  </Button>
-                </Left>
-                <Body>
-                  <Button transparent>
-                    <Icon active name="chatbubbles" />
-                    <Text>4 Comments</Text>
-                  </Button>
-                </Body>
-                <Right>
-                  <Text>11h ago</Text>
-                </Right>
-              </CardItem>
-            </Card>          
+          <TipsBlock />       
         </Content>        
         <Footer>
           <FooterTab>
---
+++ components/TipsBlock.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Card, CardItem, Thumbnail, Left, Body, Right, Text, Button, Icon } from 'native-base';
+
+export class TipsBlock extends Component {
+
+  render() {
+    return (
+      <Card>
+        <CardItem>
+          <Left>
+            <Thumbnail source={{uri: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&imgrefurl=https://www.pexels.com/search/nature/&docid=ShwNVOdFBcmkxM&tbnid=8c_UAo3gH_220M:&vet=1&w=500&h=200&source=sh/x/im'}} />
+            <Body>
+              <Text>NativeBase</Text>
+              <Text note>GeekyAnts</Text>
+            </Body>
+          </Left>
+        </CardItem>
+        <CardItem cardBody>
+          <Image source={{uri: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&imgrefurl=https://www.pexels.com/search/nature/&docid=ShwNVOdFBcmkxM&tbnid=8c_UAo3gH_220M:&vet=1&w=500&h=200&source=sh/x/im'}} style={{height: 200, width: null, flex: 1}}/>
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Button transparent>
+              <Icon active name="thumbs-up" />
+              <Text>12 Likes</Text>
+            </Button>
+          </Left>
+          <Body>
+            <Button transparent>
+              <Icon active name="chatbubbles" />
+              <Text>4 Comments</Text>
+            </Button>
+          </Body>
+          <Right>
+            <Text>11h ago</Text>
+          </Right>
+        </CardItem>
+      </Card>
+    )
+  }
+}
