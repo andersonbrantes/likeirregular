0a577df31612d9d51f2eacc3c5278e22b6398199
Anderson P. Brantes <andersonbrantes@gmail.com>
2019-02-24 11:54:56 -0300

    Creates a data file for Irregular Verbs Store

---
+++ components/data/VerbsData.js
@@ -0,0 +1,22 @@
+export const verbs  = [
+    {
+        infinitive: 'awake',
+        simplePast: 'awoke',
+        pastParticiple: 'awoken'
+    },
+    {
+        infinitive: 'be',
+        simplePast: ' was',
+        pastParticiple: 'been'
+    },
+    {
+        infinitive: 'beat',
+        simplePast: 'beat',
+        pastParticiple: 'beaten'
+
+    }
+  ]
+  
+export default {
+    verbs
+}
