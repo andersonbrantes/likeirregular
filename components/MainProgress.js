d7a8cdc2d92758d0599871263a69653ef9b627e8
Anderson P. Brantes <andersonbrantes@gmail.com>
2019-02-24 09:43:35 -0300

    Adds component to track progress

---
+++ components/MainProgress.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Text } from 'native-base';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
+
+export class MainProgress extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      visibleModal: null,
+      fill: 75 // %
+    };
+
+    render() {
+        return (
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection:'row', marginTop: 80, marginBottom: 50 }}>
+            <AnimatedCircularProgress
+              size={120}
+              width={15}
+              fill={this.state.fill}
+              tintColor="#00e0ff"
+              onAnimationComplete={() => console.log('onAnimationComplete')}
+              backgroundColor="#3d5875"
+            >
+              {
+                (fill) => (
+                  <Text>
+                    { this.state.fill }
+                  </Text>
+                )
+              }
+            </AnimatedCircularProgress>
+          </View>            
+        )
+    }
+}
