36b113e4e6f7a4df404be0f1978b4eb7c8be0b96
Anderson P. Brantes <andersonbrantes@gmail.com>
2019-04-06 14:18:55 -0300

    Move the method which selects the active verb into the AnswerBlock component.

--- components/AnswerBlock.js
+++ components/AnswerBlock.js
@@ -5,6 +5,15 @@ import { Text, Input, Left } from "native-base";
 import { MatchBlock } from "../components/MatchBlock";
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "yellow",
+    height: 300,
+    borderRadius: 4,
+    borderColor: "rgba(0, 0, 0, 0.1)",
+    flexWrap: "wrap",
+    flexDirection: 'column',
+    alignItems: 'stretch'
+  },
   verbBlock: {
     flex: 1,
     justifyContent: "center",
@@ -35,6 +44,20 @@ export class AnswerBlock extends Component {
     super(props);
   }
 
+  componentWillMount() {
+    this.getVerb();
+  }
+
+  getVerb() {   
+    if (this.props.activeVerbs.length === 0) {
+      return false;
+    }
+
+    const selectedItem = this.props.activeVerbs.splice(Math.floor(Math.random()*this.props.activeVerbs.length), 1)[0];
+
+    this.props.updateActiveVerb(selectedItem)
+  }
+
   hiddenItem() {
     const item          = this.props.activeVerb;
     const verbTense     = ["infinitive", "simplePast", "pastParticiple"];
@@ -49,7 +72,7 @@ export class AnswerBlock extends Component {
     const activeVerbTense = this.hiddenItem()[0];
 
     return (
-      <View style={{marginTop: 30}}>    
+      <View style={ styles.container }>   
         {                    
           this.props.remainingVerbs === 0 ?
           
@@ -58,23 +81,20 @@ export class AnswerBlock extends Component {
           Object.keys(activeVerb).map( (key) => {
             const hiddenVerb = key === activeVerbTense;
 
-            return (                            
-              <View key={key}>
-              {
-                hiddenVerb ?
-                ( 
-                  <MatchBlock
-                    target={ activeVerb[key] }
-                    hitOrMiss={ (r) => this.props.hitOrMiss(r) }
-                  />
-                ) :
-                ( 
-                  <View style={ [styles.verbBlock,  styles.verbBlock2] }>
-                    <Text style={ styles.verbText }>{ activeVerb[key].toUpperCase() }</Text> 
-                  </View>
-                )
-              }                                                                
-              </View>
+            return (                                                      
+              hiddenVerb ?
+              ( 
+                <MatchBlock
+                  key={key}
+                  target={ activeVerb[key] }
+                  hitOrMiss={ (r) => this.props.hitOrMiss(r) }
+                />
+              ) :
+              ( 
+                <View key={key} style={ [styles.verbBlock,  styles.verbBlock2] }>
+                  <Text style={ styles.verbText }>{ activeVerb[key].toUpperCase() }</Text> 
+                </View>
+              )                                                                                        
             );
           })
         }
